bff421719b75c8d933d0ae192e6ca791a4f179fe
Dalmas <akumuodhiambo@gmail.com>
2023-08-29 10:26:14 +0300

    Completed Assignment

--- src/App.js
+++ src/App.js
@@ -2,6 +2,48 @@ import React from "react";
 import Article from "./Article";
 import Comment from "./Comment";
 
+
+
+import React from "react";
+
+function NavBar() {
+  return (
+    <nav>
+      <a href="#home">Link!</a>
+    </nav>
+  );
+}
+
+function Home() {
+  return (
+    <div id="home page">
+      <h1>Home</h1>
+    </div>
+  );
+}
+
+function About(){
+  return(
+    <div id="about page">
+      <h2>About</h2>
+    </div>
+
+  )
+}
+
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <Home />
+      <About />
+    </div>
+  );
+}
+
+export default App;
+
 function App() {
   return (
     <div>
